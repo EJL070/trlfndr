56a36957fc9fcab98c56ca84c5055b5ddd184c62
ErikJL <74800670+EJL070@users.noreply.github.com>
2022-10-28 16:55:42 +0200

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,38 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const listener = async(req,res)=>{
+   
+    return new Response(req.url);
+    
+}
+
+
+//   if (pathname.startsWith("/style.css")) {
+//     // Read the style.css file from the file system.
+//     const file = await Deno.readFile("./style.css");
+//     // Respond to the request with the style.css file.
+//     return new Response(file, {
+//       headers: {
+//         "content-type": "text/css",
+//       },
+//     });
+//   }
+
+//   return new Response(
+//     `<html>
+//       <head>
+//         <link rel="stylesheet" href="style.css" />
+//       </head>
+//       <body>
+//         <h1>Example</h1>
+//       </body>
+//     </html>`,
+//     {
+//       headers: {
+//         "content-type": "text/html; charset=utf-8",
+//       },
+//     },
+//   );
+// }
+
+serve(listener);
